f1aaf6bb5b65ba6f439c8957ea580933a4f206da
Tim <timmorgner@gmail.com>
2020-03-02 00:53:35 +0100

    Auth roleRequired OR capabilities

--- src/api/middlewares/AuthenticationMiddleware.js
+++ src/api/middlewares/AuthenticationMiddleware.js
@@ -3,7 +3,7 @@ db = require("../../data/db.js");
 exports.roleRequired = (...roles) => {
     return (req, res, next) => {
         for (let i = 0; i < arguments.length; i++) {
-            if (db.hasUserRole(req.user_id, roles))
+            if (db.hasUserRole(req.user_id, roles[i]))
                 return next();
         }
         return res.status(403).send({code: "403", message: "Ungültige Rolle"});
--- src/telegram/utils/AuthenticationCheck.js
+++ src/telegram/utils/AuthenticationCheck.js
@@ -1,11 +1,11 @@
 db = require("../../data/db.js");
 
-exports.roleRequired = (role) => {
+exports.roleRequired = (...roles) => {
     return (ctx, next) => {
-        if (db.hasUserRole(ctx.from.id, role)) {
-            next();
-        } else {
-            ctx.reply("Du hast keine Berechtigung dazu.");
+        for (let i = 0; i < roles.length; i++) {
+            if (db.hasUserRole(ctx.from.id, roles[i]))
+                return next();
         }
+        ctx.reply("Du hast keine Berechtigung dazu.");
     }
 };
