4426d5e364b97838da9957847c03f76fff120cd3
CookieJ4R <cookiej4r@web.de>
2020-03-02 20:24:13 +0100

    fixed randomCard.js

--- src/telegram/commands/randomCard.js
+++ src/telegram/commands/randomCard.js
@@ -1,11 +1,11 @@
 const scry = require("scryfall-sdk");
 
-exports.setupRandomCard = this.setupRandomCard;
+exports.setupRandomCard = setupRandomCard;
 
 function setupRandomCard(bot) {
     bot.command("randomCard", (ctx) => {
         scry.Cards.random().then(card => {
-            ctx.reply("Hier ist eine zufällige Karte: <a href=\"" + card.image_uris.small + "\">" + card.name + "</a>\n");
+            ctx.reply("Hier ist eine zufällige Karte: <a href=\"" + card.image_uris.normal + "\">" + card.name + "</a>\n", {parse_mode: "HTML"});
         });
     });
 }
